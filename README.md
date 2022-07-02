3696e2839d359e3ed06d68afb24ed0e1dc471a95
Vinícius <matosvb@gmail.com>
2022-07-02 19:07:37 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+### Fix WA Web Time Format
+
+This is a small extension that fixes the time format on WhatsApp Web.
+
+For months, WhatsApp Web has been showing timestamps in 12-hour format
+when the language is set to English, rather than the expected
+24-hour format for locales other than en-US.
---
+++ fix-wa-web-time-format/main.js
@@ -0,0 +1,51 @@
+const fixFormat = (text) =>
+  text.replace(/(\d?\d):(\d\d) ([ap])m/i, (match, h, min, ap) => {
+    h = parseInt(h);
+    const pm = ap.toLowerCase() == "p";
+    if (pm && h != 12) h += 12;
+    else if (!pm && h == 12) h = 0;
+    return String(h).padStart(2, "0") + ":" + min;
+  });
+
+const fixTextNode = (textNode) => {
+  const fixed = fixFormat(textNode.textContent);
+  if (fixed !== textNode.textContent) {
+    textNode.textContent = fixed;
+  }
+  const obs = new MutationObserver((mutations) => {
+    mutations.forEach(
+      (mutation) =>
+        mutation.type === "characterData" && fixTextNode(mutation.target)
+    );
+  });
+  obs.observe(textNode, {
+    characterData: true,
+  });
+};
+
+const fixNode = (node) =>
+  node.nodeType === Node.ELEMENT_NODE &&
+  node
+    .querySelectorAll(
+      [
+        ".fewfhwl7", // message
+        "._1i_wG", // last message (on the left)
+        "._2YPr_", // last seen
+        "._1qB8f", // status, message read, message delivered
+      ].join(",")
+    )
+    .forEach((e) => {
+      if (
+        e.childNodes.length == 1 &&
+        e.childNodes[0].nodeType == Node.TEXT_NODE
+      ) {
+        fixTextNode(e.childNodes[0]);
+      }
+    });
+
+const observer = new MutationObserver((mutations) =>
+  mutations.forEach((mutation) => {
+    mutation.addedNodes.forEach(fixNode);
+  })
+);
+observer.observe(document.body, { childList: true, subtree: true });
